38654aa9573fa2a8a3497be2fbf1a9965f0831ab
gdo654 <gdo654@gmail.com>
2025-08-04 19:58:08 +0900

    feat(business): 비즈니스 앱 폴더 구조 구성 및 core 폴더 제거

--- backend/pickomain/urls.py
+++ backend/pickomain/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/core/', include('core.urls')),
+    path('api/v1/business/', include('business.urls')),
 ]
