d4bd73a7abb754199e2ce0b117180e6df7976670
soran0505 <goeune03@naver.com>
2025-08-06 09:52:53 +0900

    print customers

--- backend/business/urls.py
+++ backend/business/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import BusinessCreateAPIView
+from .views import BusinessCreateAPIView, BusinessListAPIView
 
 urlpatterns = [
     path('auth/register/', views.register_user, name='register_user'),
     path('auth/status/', views.check_user_status, name='check_user_status'),
     path('customers/', BusinessCreateAPIView.as_view()),
+    path('customerslist/', BusinessListAPIView.as_view()), 
 ] 
